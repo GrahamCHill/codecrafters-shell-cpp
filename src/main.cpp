3a4f93a883137848c442a470021040c941822852
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-06 19:20:12 +0200

    Looping Commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while (true) {
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cerr << input << ": command not found" << std::endl;
+    std::string input;
+    std::getline(std::cin, input);
+    std::cerr << input << ": command not found" << std::endl;
+  }
+
 }
