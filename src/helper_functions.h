ec110593538c3c53e1e5f387be46b93a7067c3f1
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-07 01:10:51 +0200

    cd basic for testing

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -117,6 +117,7 @@ inline int exit_command(std::istringstream& input_string) {
 
 inline void attempt_exec_command(std::istringstream &input_string, const std::string &input, const std::string& first_word) {
     std::string second_word;
+    input_string >> second_word;
     if (first_word == "cd") {
         // Call the built-in `cd_command`
         cd_command(second_word);
