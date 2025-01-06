6666b17386e074f31635286b7783e6b55c95cd0e
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-06 19:27:27 +0200

    Echo Command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,10 +11,18 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
-    if (input == "exit 0") {
+    std::istringstream iss(input);
+    std::string first_word;
+    iss >> first_word;
+    if (input == "exit 0")
       return 0;
+    if (first_word == "echo") {
+      std::string remaining;
+      std::getline(iss, remaining);
+      std::cout << remaining << std::endl;
     }
-    std::cerr << input << ": command not found" << std::endl;
+    else
+      std::cerr << input << ": command not found" << std::endl;
   }
 
 }
